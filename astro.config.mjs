fb3a7867daa6999431e14738da1ed5aede653836
Jefferson <jeff.silva.caires@gmail.com>
2025-11-23 10:45:04 -0300

    refactor: changed home animation icosehadron to sphere to make it more performatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), partytown()],
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   },
